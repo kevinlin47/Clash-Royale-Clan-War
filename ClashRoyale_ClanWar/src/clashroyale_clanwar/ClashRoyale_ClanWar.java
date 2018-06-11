35a112729274d02d65e836d72a188143037a9c5a
Kevin Lin <kevinlin47@hotmail.com>
2018-06-11 10:57:22 -0400

    Removed ability to resize the columns
    Only in the war state tab

--- ClashRoyale_ClanWar/src/clashroyale_clanwar/ClashRoyale_ClanWar.java
+++ ClashRoyale_ClanWar/src/clashroyale_clanwar/ClashRoyale_ClanWar.java
@@ -28,6 +28,7 @@ import javafx.collections.ObservableList;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
@@ -193,8 +194,17 @@ public class ClashRoyale_ClanWar extends Application {
         sessionWinsCol.setSortable(false);
         totalWinsCol.setSortable(false);
         winLoseCol.setSortable(false);
+        statNameCol.setResizable(false);
+        sessionWinsCol.setResizable(false);
+        totalWinsCol.setResizable(false);
+        winLoseCol.setResizable(false);
+        
+        BorderPane borderPane=new BorderPane();
+        borderPane.setCenter(statTable);
+        statTable.getColumns().addAll(statNameCol,sessionWinsCol,totalWinsCol,winLoseCol);
         
         memberTab.setContent(grid);
+        warTab.setContent(borderPane);
         
         Scene scene=new Scene(tabPane,800,800);
         
